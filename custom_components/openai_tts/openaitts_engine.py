e6044a7d385330168e918780788e5aefe6fd86da
Max R <mxr@users.noreply.github.com>
2024-12-06 11:16:25 -0500

    Update openaitts_engine.py

--- custom_components/openai_tts/openaitts_engine.py
+++ custom_components/openai_tts/openaitts_engine.py
@@ -16,7 +16,7 @@ class OpenAITTSEngine:
             "model": self._model,
             "input": text,
             "voice": self._voice,
-            "response_format": "wav",
+            "response_format": "mp3",
             "speed": self._speed
         }
         return requests.post(self._url, headers=headers, json=data)
